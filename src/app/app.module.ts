c15a0991492338aff6dc8042ed2e9c627e979f0c
Marcot132 <marco.corona547@gmail.com>
2023-09-15 00:22:27 -0600

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +16,10 @@ import { WorkExperienceComponent } from './work-experience/work-experience.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+	
+   constructor(private headerService: HeaderService) {}
 
+   name : string = "Marco";
+   titulos : string = "P7";
+   mision : string = "ser competitivo internacionalmente";
+   foto : string = "";
+   email : string = "marco.corona547@gmail.com";
+   celular : string = "(272)";
+   ubicacion : string = "Orizaba Veracruz, MEX";
+   redsocial : string = "@"
+
+   ngOnInit(): void {
+	 this.headerService.getHeader()
+	 .subscribe( (data: any) => {
+	   console.log(data);
+	   //alert(data);
+	   this.name = data.name;
+	   this.titulos = data.titulos;
+     this.mision = data.mision;
+	   this.foto = data.foto;
+     this.email = data.email;
+     this.celular = data.celular;
+     this.ubicacion = data.ubicacion;
+     this.redsocial = data.redsocial;
+
+	   });
+   }
 }
